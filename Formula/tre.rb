7056764e9ec2cfb7a44cd0b1f31bed4dea8ec863
Daniel Duan <daniel@duan.ca>
2020-06-15 19:25:53 -0700

    [tre] Release 0.3.1

--- Formula/tre.rb
+++ Formula/tre.rb
@@ -1,14 +1,14 @@
 class Tre < Formula
-  version '0.3.0'
+  version '0.3.1'
   desc "Tree command, improved."
   homepage "https://github.com/dduan/tre"
 
   if OS.mac?
       url "https://github.com/dduan/tre/releases/download/v#{version}/tre-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "0e15f7aeb3385c4bd6bfc63bbc9a964dc93a8e2e3b66ef5d950ca70c1831d1e0"
+      sha256 "1b15ac191a95baa1ba6e70cec89cdb9dbcb172764805c52a1951153cf9574a27"
   elsif OS.linux?
       url "https://github.com/dduan/tre/releases/download/v#{version}/tre-v#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "1c9f76f2cd6962d43a94e89006f386c1d06f1d7e7c0b6d47e91f4e738feffb4a"
+      sha256 "5f0353764855cf8256dc69c9ae8a126c937cb728d60a144c23c0765d2492964b"
   end
 
   def install
