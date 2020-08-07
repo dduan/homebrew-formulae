8a89fa8e7fd1319a867da4662f058251b37faffc
Daniel Duan <daniel@duan.ca>
2020-08-06 23:20:12 -0700

    [drstring] Release 0.4.2

--- Formula/drstring.rb
+++ Formula/drstring.rb
@@ -1,23 +1,25 @@
 class Drstring < Formula
-   version '0.4.1'
-   desc "Linter, formatter for Swift docstrings."
-   homepage "https://github.com/dduan/drstring"
+  version '0.4.2'
+  desc "Linter, formatter for Swift docstrings."
+  homepage "https://github.com/dduan/drstring"
 
-   if OS.mac?
-       url "https://github.com/dduan/DrString/releases/download/v#{version}/drstring-x86_64-apple-darwin.tar.gz"
-       sha256 "49fc7b630ec6f1e3711d4013407223edbc3540a4e05bc2b7e4da0467df5f879f"
-   elsif OS.linux?
-       url "https://github.com/dduan/DrString/archive/v#{version}.tar.gz"
-       sha256 "3df1725902a65a0ac17e0c6b6f89f126d32c3c9432cf6ef9e5dfd587fb4b8165"
-   end
+  if OS.mac?
+    url "https://github.com/dduan/DrString/releases/download/v#{version}/drstring-x86_64-apple-darwin.tar.gz"
+    sha256 "081f5ded9f884db0fce02754b980a36755156d7cb3ca25a32a7ac7fe6d434823"
+  elsif OS.linux?
+    url "https://github.com/dduan/DrString/archive/v#{version}.tar.gz"
+    sha256 "1f5f6304a8ec03251b13696600ac0400e2ac8df11e9210af214009157889e73a"
+  end
 
-   def install
-       if OS.mac?
-         bin.install "bin/drstring"
-         lib.install "lib/lib_InternalSwiftSyntaxParser.dylib"
-       elsif OS.linux?
-         system "make", "build"
-         bin.install ".build/release/drstring"
-       end
-   end
- end
+  def install
+    if OS.mac?
+      bin.install "bin/drstring"
+      lib.install "lib/lib_InternalSwiftSyntaxParser.dylib"
+    elsif OS.linux?
+      system "make", "build"
+      bin.install ".build/release/drstring"
+    end
+    bash_completion.install "completions/bash/drstring-completion.bash"
+    zsh_completion.install "completions/zsh/_drstring"
+  end
+end
