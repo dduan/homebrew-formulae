39caa1cc44e05f14a31f92cfdffa365b9931f2a3
Daniel Duan <daniel@duan.ca>
2022-05-26 23:45:07 -0700

    [ea] Release 0.2.0

--- Formula/ea.rb
+++ Formula/ea.rb
@@ -1,8 +1,8 @@
 class Ea < Formula
   desc "Ea: make your CLI output actionable"
   homepage "https://github.com/dduan/ea"
-  url "https://github.com/dduan/ea/archive/0.1.0.tar.gz"
-  sha256 "269d57cf78645bd2b238bd3dbde965c05d1be631bcaffc354d9fcff4c77e06e7"
+  url "https://github.com/dduan/ea/archive/0.2.0.tar.gz"
+  sha256 "df461668c4603ec01614e5c680ff8e511451d4055f4f4572edcd2fe2fa3aca1e"
   license "MIT"
   head "https://github.com/dduan/ea.git"
 
@@ -12,6 +12,10 @@ class Ea < Formula
     system "cargo", "install", "--locked",
                                "--root", prefix,
                                "--path", "."
+    man1.install "docs/ea.1"
+    bash_completion.install "scripts/completion/ea.bash"
+    fish_completion.install "scripts/completion/ea.fish"
+    zsh_completion.install "scripts/completion/_ea"
   end
 
   test do
